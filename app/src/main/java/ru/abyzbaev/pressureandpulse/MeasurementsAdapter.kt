0f7e69eb2f8780658fffbd39013433fa116f3184
Timur <timur@vlaga.pro>
2024-02-22 17:29:18 +0500

    Изменил структуру для группировки по дате

--- app/src/main/java/ru/abyzbaev/pressureandpulse/MeasurementsAdapter.kt
+++ app/src/main/java/ru/abyzbaev/pressureandpulse/MeasurementsAdapter.kt
@@ -4,42 +4,66 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MeasurementsAdapter(var measurements: List<Measurement>) : RecyclerView.Adapter<MeasurementsAdapter.ViewHolder>() {
+class MeasurementsAdapter(var measurementGroups: List<MeasurementGroup>) : RecyclerView.Adapter<MeasurementsAdapter.MeasurementGroupViewHolder>() {
 
-    init {
-        val sortedMeasurements = measurements.sortedByDescending { it.timestamp }
-        // Присвойте отсортированный список переменной класса
-        this.measurements = sortedMeasurements
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MeasurementGroupViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.measurement_group_item, parent, false)
+        return MeasurementGroupViewHolder(itemView)
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val pressureTextView: TextView = view.findViewById(R.id.pressureTextView)
-        val pulseTextView: TextView = view.findViewById(R.id.pulseTextView)
-        val timestampTextView: TextView = view.findViewById(R.id.timestampTextView)
-        val timestampTimeTextView: TextView = view.findViewById(R.id.timestampTimeTextView)
+    override fun onBindViewHolder(holder: MeasurementGroupViewHolder, position: Int) {
+        val measurementGroup = measurementGroups[position]
+        holder.bind(measurementGroup)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_measurement, parent, false)
-        return ViewHolder(view)
+    override fun getItemCount(): Int {
+        return measurementGroups.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    // ViewHolder для элементов списка
+    class MeasurementGroupViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(measurementGroup: MeasurementGroup) {
+            // Привяжите данные к ViewHolder для отображения в RecyclerView
+            itemView.findViewById<TextView>(R.id.dateTextView).text = measurementGroup.date
+            //itemView.dateTextView.text = measurementGroup.date
+
+            // Создайте и настройте внутренний RecyclerView для отображения измерений для данной даты
+            val measurementsRecyclerView = itemView.findViewById<RecyclerView>(R.id.measurementsRecyclerView)
+            measurementsRecyclerView.layoutManager = LinearLayoutManager(itemView.context)
+            val measurementsAdapter = MeasurementsInnerAdapter(measurementGroup.measurements)
+            measurementsRecyclerView.adapter = measurementsAdapter
+        }
+    }
+}
+
+// Внутренний адаптер RecyclerView для отображения измерений для каждой даты
+class MeasurementsInnerAdapter(private val measurements: List<Measurement>) : RecyclerView.Adapter<MeasurementsInnerAdapter.MeasurementViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MeasurementViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.measurement_item, parent, false)
+        return MeasurementViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MeasurementViewHolder, position: Int) {
         val measurement = measurements[position]
-        holder.pressureTextView.text = "${measurement.upperPressure}  /  ${measurement.lowerPressure}"
-        holder.pulseTextView.text = "${measurement.pulse}"
-val date = Date(measurement.timestamp)
-        val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
-        val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-        holder.timestampTextView.text = dateFormat.format(date)
-        holder.timestampTimeTextView.text = timeFormat.format(date)
+        holder.bind(measurement)
     }
 
     override fun getItemCount(): Int {
         return measurements.size
+    }
+
+    // ViewHolder для элементов списка
+    class MeasurementViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(measurement: Measurement) {
+            itemView.findViewById<TextView>(R.id.timestampTimeTextView).text = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date(measurement.timestamp))
+            itemView.findViewById<TextView>(R.id.pressureTextView).text = "${measurement.upperPressure}  /  ${measurement.lowerPressure}"
+            itemView.findViewById<TextView>(R.id.pulseTextView).text = "${measurement.pulse}"
+        }
     }
 }
